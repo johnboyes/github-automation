ac756ed2aaf2714d371a5105d01eefbb3deaca9c
John Boyes <john@edinburghwebservices.co.uk>
2015-07-23 15:19:50 +0100

    methods will now run when called

--- github-automation.rb
+++ github-automation.rb
@@ -5,12 +5,12 @@ $stdout.sync = true #so we can see stdout when starting with foreman, see https:
 ORG=ENV['ORG']
 TEAM_ID=ENV['TEAM_ID']
 
-client = Octokit::Client.new(:access_token => ENV['ACCESS_TOKEN'], :auto_paginate => true)
+CLIENT = Octokit::Client.new(:access_token => ENV['ACCESS_TOKEN'], :auto_paginate => true)
 
 def add_all_repositories_except_excluded_to_team
-  client.org_repos(ORG).each { |repository| client.add_team_repository(TEAM_ID, repository.full_name) unless repository.name == ENV['EXCLUDE'] }
+  CLIENT.org_repos(ORG).each { |repository| CLIENT.add_team_repository(TEAM_ID, repository.full_name) unless repository.name == ENV['EXCLUDE'] }
 end
 
 def add_all_members_in_org_to_team
-  client.org_members(ORG).each { |org_member| client.add_team_member(TEAM_ID] org_member.login) }
+  CLIENT.org_members(ORG).each { |org_member| CLIENT.add_team_member(TEAM_ID, org_member.login) }
 end
