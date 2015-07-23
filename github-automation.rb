e8f5365f474c3fd472aaca0f2fcfe115270a82fb
John Boyes <john@edinburghwebservices.co.uk>
2015-07-23 15:07:02 +0100

    extracted explaining methods

--- github-automation.rb
+++ github-automation.rb
@@ -7,6 +7,10 @@ TEAM_ID=ENV['TEAM_ID']
 
 client = Octokit::Client.new(:access_token => ENV['ACCESS_TOKEN'], :auto_paginate => true)
 
-client.org_repos(ORG).each { |repository| client.add_team_repository(TEAM_ID, repository.full_name) unless repository.name == ENV['EXCLUDE'] }
+def add_all_repositories_except_excluded_to_team
+  client.org_repos(ORG).each { |repository| client.add_team_repository(TEAM_ID, repository.full_name) unless repository.name == ENV['EXCLUDE'] }
+end
 
-client.org_members(ORG).each { |org_member| client.add_team_member(TEAM_ID] org_member.login) }
+def add_all_members_in_org_to_team
+  client.org_members(ORG).each { |org_member| client.add_team_member(TEAM_ID] org_member.login) }
+end
